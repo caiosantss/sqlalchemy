193464e2f67fbaaf50e7e39425cbaccd096d7ac7
Caio Santos <caiosilvasantos87@gmail.com>
2026-08-08 14:13:42 -0300

    feat: Query builder, metadata - conceitos do CORE

--- examples/ex_04.py
+++ examples/ex_04.py
@@ -1,28 +1,34 @@
 import os
 
-from sqlalchemy import create_engine, text, inspect, Table, MetaData
+from sqlalchemy import (
+    create_engine, Table, MetaData, select, insert
+)
 
 engine = create_engine(str(os.getenv('DB_PATH')), echo=True)
-inspected = inspect(engine)
+
 metadata = MetaData()
 
 table = Table(
     'users_test',
     metadata,
-    autoload_with=engine # Reflection: _schema.Table object will be reflected
+    autoload_with=engine # Reflection
 )
 
-#Refatorar
+#DQL
+#Query builder
+sql = (
+        select(table.columns.id, table.columns.name)
+       .where(table.columns.name == 'Ambriosio')
+)
+
+#DML
+sql_insert = (
+        insert(table)
+        .values(name='Ambriosio')
+)
+
 with engine.connect() as conn:
     with conn.begin():
-        query_create = text('CREATE TABLE IF NOT EXISTS users_test (id INTEGER PRIMARY KEY, name TEXT UNIQUE)')
-        conn.execute(query_create)
-
-    with conn.begin():
-        query_insert = text('INSERT INTO users_test (name) VALUES (:name) ON CONFLICT (name) DO NOTHING')
-        conn.execute(query_insert, {'name': 'Alice'})
-
-    with conn.begin():
-        query_select = text('SELECT * FROM users_test')
-        result = conn.execute(query_select)
+        conn.execute(sql_insert)
+        result = conn.execute(sql)
         print(result.fetchall())
